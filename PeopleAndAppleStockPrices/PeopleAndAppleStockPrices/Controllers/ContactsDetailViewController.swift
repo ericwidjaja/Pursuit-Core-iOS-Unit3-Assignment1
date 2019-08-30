a45d2aec36659db94994026bc4154f51d157a8fe
Eric Widjaja <ericwidjaja@pursuit.org>
2019-08-30 18:03:17 -0400

    Create RandomUsers Model File, created 1 tab VC, 2 NavVC, 4 VCs

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/ContactsDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ContactsDetailViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Eric Widjaja on 8/30/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+
+import UIKit
+
+class ContactsDetailViewController: UIViewController {
+    
+    
+    
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/RandomUKUsers.swift
@@ -0,0 +1,36 @@
+//
+//  RandomUKUsers.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Eric Widjaja on 8/30/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+//MARK-Create Structs from json file.
+struct RandomUKUsers: Codable {
+    let reults: [ContactInfo]
+}
+
+struct ContactInfo: Codable {
+    let name: NameWrapper
+    let location: LocationWrapper
+    let picture: PictureWrapper
+    let email: String
+}
+
+struct NameWrapper: Codable {
+    let title: String
+    let first: String
+    let last: String
+}
+
+struct LocationWrapper: Codable {
+    let state: String
+}
+
+struct PictureWrapper:Codable {
+    let large: String
+    let medium: String
+    let thumbnail: String
+}
